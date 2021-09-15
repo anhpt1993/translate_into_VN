ee88ffd73f8060db58b064548fd543a2cbaa52f5
anhpt1993 <88441530+anhpt1993@users.noreply.github.com>
2021-09-15 21:28:58 +0700

    2128_150921 revise translate.py file

--- translate.py
+++ translate.py
@@ -1,17 +1,18 @@
 dict = {"bear":"con gấu", "elephant":"con voi", "fox": "con cáo", "jaguar":"báo đốm", "lion":"sư tử", "raccoon":"gấu mèo"}
 
-def input_data(text):
+def input_word(text):
     return input(text)
 
 def is_in_dict(text, dict):
     for key in dict:
         if key == text:
             return True
-        else:
-            return False
+            break
+    else:
+        return False
 
 if __name__ == '__main__':
-    word = input_data("Enter the word to be translated into Vietnamese: ")
+    word = input_word("Enter the word to be translated into Vietnamese: ")
     if is_in_dict(word, dict):
         print(f"'{word}' : '{dict[word]}'")
     else:
